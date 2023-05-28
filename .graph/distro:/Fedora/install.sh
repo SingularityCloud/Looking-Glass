e9314e4cfb8f199bb14b20a89cdd95f9d4bb9da8
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-05-28 13:24:40 -0500

    Update install.sh

--- .graph/distro:/Fedora/install.sh
+++ .graph/distro:/Fedora/install.sh
@@ -1,3 +1,8 @@
+# Fix permissions
 echo ```# Type Path               Mode UID  GID Age Argument
 
-f /dev/shm/looking-glass 0660 me qemu -``` | sudo tee /etc/tmpfiles.d/10-looking-glass.conf
+f /dev/shm/looking-glass 0660 qemu qemu -``` | sudo tee /etc/tmpfiles.d/10-looking-glass.conf
+
+sudo chown qemu:qemu /dev/shm/looking-glass
+
+usermod -a -G $USER qemu
